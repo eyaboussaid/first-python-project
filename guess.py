07de762432f70a5d1c29e8b93c97df1343e44d32
Eya AI <eyaboussaid.3@gmail.com>
2026-06-07 20:15:38 +0100

    Update guess.py2

--- guess.py
+++ guess.py
@@ -1,8 +1,11 @@
 import random
 number = random.randint(1, 10)
-guess = int(input("Guess a number between 1 and 10: "))
-
-if guess == number:
-    print("Correct 🎉")
-else:
-    print("Wrong 😅 The number was", number)
+guess = -1
+while guess != number:
+    guess = int(input("Guess a number (1-10): "))
+    if guess < number:
+        print("Too low 📉")
+    elif guess > number:
+        print("Too high 📈")
+    else:
+        print("Correct 🎉 You win!")
